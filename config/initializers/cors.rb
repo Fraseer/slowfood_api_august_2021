878c594d19a58de22d6541c39cf7ba936bbb244c
maxarvid <max.arvid.anderson@gmail.com>
2021-10-10 18:06:44 +0200

    Removes wildcard form CORS config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://too-gross-for-comfort.netlify.app*'
 
     resource '*',
       headers: :any,
